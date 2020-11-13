0116d0df0b4629e591e991bcbb29291abda22f5a
JoseMartinNogueira <jmartin.nogueira@gmail.com>
2020-11-13 17:41:01 +0100

    Looking for Translation API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import React, {useState} from 'react'
+import axios from 'axios'
 import './App.css';
 import Col from 'react-bootstrap/esm/Col';
 
+
 function App() {
   
-  const [textValue, setTextValue] = useState("");
+  const [textValue, setTextValue] = useState("")
   const [translated, setTranslated] = useState("")
+  const [language, setLanguage] = useState("es")
 
   const translation = () => {
-    setTranslated(textValue.replace(/[aeou]/ig, 'i'))
+    //axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key='+key+'&lang='+language+'&text='+textValue).then((response) =>
+    //setTranslated(response.data.text[0]))
+    
   }
-
   const handleChange = (event) => {
-    setTextValue(event.target.value);
-  };
-console.log(textValue);
+    setTextValue(event.target.value)
+  }
+  const setLanguageTranslation = (event) => {
+    setLanguage(event.target.value)
+  }
+console.log(language);
   return (
     <div className="App">
-    <Form.Group>
+    <Form.Group c>
       <Form.Row>
         <Form.Label column='lg' lg={2}>
           Input Text
@@ -29,17 +36,22 @@ console.log(textValue);
           <Form.Control as="textarea" rows={4} className= "big-text" value={textValue} onChange={handleChange}/>
         </Col>
       </Form.Row>
-      <Button onClick={translation}>
-        Translate
-      </Button>
+      <Form.Control as="select" className="nice-select" multiple onChange={setLanguageTranslation}>
+        <option value="es"> Spanish </option>
+        <option value="fr"> French </option>
+        <option value="ru"> Russian </option>   
+      </Form.Control> 
       <Form.Row>
+      <br/>
+        <Button onClick={translation}>
+          Translate
+        </Button>
+      <br/>
         <Form.Label column="lg" lg={2}>
             Translated Text
         </Form.Label>
         <Col>
-          <Form.Control as="textarea" rows={4}  value={translated} className="big-text" readOnly>
-            
-          </Form.Control>
+          <Form.Control as="textarea" rows={4}  value={translated} className="big-text" readOnly/>
         </Col>
       </Form.Row>
     </Form.Group>
