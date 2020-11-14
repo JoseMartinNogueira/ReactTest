f6e6e3ed72a64efb0c0e4aec9edb01fe699c1913
JoseMartinNogueira <jmartin.nogueira@gmail.com>
2020-11-14 20:13:15 +0100

    sentiment api working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import React, {useState} from 'react'
+import axios from 'axios'
 import './App.css';
 import Col from 'react-bootstrap/esm/Col';
 
 function App() {
+
+  const [textValue, setTextValue] = useState("");
+  const [sentiment, setSentiment] = useState("")
+
+  const sentimentResult = () => {
+    let apiRequest = {
+      method: 'post',
+      url: 'https://sentim-api.herokuapp.com/api/v1/',
+      headers: {
+        'Accept': "application/json", 
+        "Content-Type": "application/json"
+      },
+      data: { 
+        "text": textValue 
+      },
+    }
+    axios(apiRequest).then(res =>{
+      setSentiment(res.data.result.type)
+    })
+  }
+
+  const handleChange = (event) => {
+    setTextValue(event.target.value);
+  };
+
   return (
     <div className="App">
     <Form.Group>
@@ -13,20 +40,19 @@ function App() {
           Input Text
         </Form.Label>
         <Col>
-          <Form.Control as="textarea" rows={4} className= "big-text"/>
+          <Form.Control as="textarea" rows={4} className= "big-text" value={textValue} onChange={handleChange}/>
         </Col>
       </Form.Row>
-      <Button>
-        Translate
+      <Button onClick={sentimentResult}>
+        Sentiment Check
       </Button>
       <Form.Row>
         <Form.Label column="lg" lg={2}>
-            Translated Text
+            Sentiment Check Result
         </Form.Label>
         <Col>
-          <Form.Control as="textarea" rows={4}  className="big-text" readOnly>
-            
-          </Form.Control>
+          <Form.Control as="textarea" rows={4}  className="big-text" value={sentiment} readOnly/>
+          
         </Col>
       </Form.Row>
     </Form.Group>
